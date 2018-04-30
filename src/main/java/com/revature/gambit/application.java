4d40e3b14ee2a1e7618be0b08e022e4e0d371717
Stephen Lovick <enrico.lovick@gmail.com>
2018-04-30 11:22:26 -0400

    Updated application.yml for eureka, added Testclasses.

--- src/main/java/com/revature/gambit/application.java
+++ src/main/java/com/revature/gambit/application.java
@@ -3,19 +3,19 @@ package com.revature.gambit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.context.annotation.Bean;
-//
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @SpringBootConfiguration
 //@EnableSwagger2
-//@EnableEurekaClient
+@EnableEurekaClient
 public class application {
 	
 	public static void main(String[] args) throws Exception {
---
+++ src/test/java/com/revature/serviceTest/BuildingTest.java
@@ -0,0 +1,34 @@
+package com.revature.serviceTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BuildingTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFindAllBuilding() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testFindBuildingByLocationId() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testFindBuildingByBuildingID() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSaveBuilding() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
